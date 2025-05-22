4b951b402b9c04e3db42cd346258139484bc9df1
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2025-05-22 19:27:15 +0530

    2025-05-22 Update Multi Region Logic

---
+++ sb-backend-module/src/main/resources/liquibase/DC-R1/scripts/user-data.sql
@@ -0,0 +1,16 @@
+/**
+ * Author:  brijeshdhaker
+ * Created: Jan 12, 2021
+ */
+
+insert into USERS (USERNAME, EMAIL, STATUS, ADD_TS, UPD_TS ) values
+('Brijesh Dhaker R1', 'brijeshdhaker@gmail.com', 'ACTIVE', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+insert into USERS (USERNAME, EMAIL, STATUS, ADD_TS, UPD_TS ) values
+('Neeta Dhaker R1', 'neetadhaker@gmail.com', 'ACTIVE', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+insert into USERS (USERNAME, EMAIL, STATUS, ADD_TS, UPD_TS ) values
+('Keshvi Dhaker R1', 'keshvidhaker@gmail.com', 'ACTIVE', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+insert into USERS (USERNAME, EMAIL, STATUS, ADD_TS, UPD_TS ) values
+('Tejas Dhaker R1', 'tejasdhaker@gmail.com', 'ACTIVE', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
