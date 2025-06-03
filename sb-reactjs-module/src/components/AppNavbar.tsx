f7b0fc4d6a8cbf8b6b45c8c796f0dd69d6375ae2
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2025-06-03 08:05:19 +0530

    2025-06-03 Update reactjs modules

--- sb-reactjs-module/src/components/AppNavbar.tsx
+++ sb-reactjs-module/src/components/AppNavbar.tsx
@@ -74,7 +74,9 @@ function AppNavbar({isAuthenticated, heading, onSelectItem}: AppNavbarProps) {
           :
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll >
-              <Nav.Link href="/">Link</Nav.Link>
+              {/*
+              <Nav.Link href="/home">Link</Nav.Link>
+              */}
             </Nav>
             <div className="col-1 d-flex justify-content-end"><SignInButton/></div>
           </Navbar.Collapse>);
--- sb-reactjs-module/vite.config.ts
+++ sb-reactjs-module/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: '/index.html',
+    open: '/',
     proxy: {
       '/api': {
         target: 'http://localhost:8100',
