4ffda66b45e0221111100a60c803e51a81b461dc
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2025-06-11 10:33:31 +0530

    2025-06-11 Update configurations

--- sb-discovery-service/src/main/java/org/examples/sb/DiscoveryService.java
+++ sb-discovery-service/src/main/java/org/examples/sb/DiscoveryService.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SbDiscoveryServiceApplication {
+public class DiscoveryService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbDiscoveryServiceApplication.class, args);
+		SpringApplication.run(DiscoveryService.class, args);
 	}
 
 }
